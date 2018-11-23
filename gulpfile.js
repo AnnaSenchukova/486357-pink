35beaf2d5d03a680aa6c4b52ba7fb9a84010f75c
AnnaSenchukova <36882792+AnnaSenchukova@users.noreply.github.com>
2018-11-23 19:34:11 +0300

    Build

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,14 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
+var rename = require ("gulp-rename");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var ccso = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -14,13 +19,16 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(ccso())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -31,4 +39,43 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("images", function () {
+return gulp.src("source/img/**/*.{png, jpg, svg}")
+.pipe(imagemin([
+imagemin.optipng({optimizationLevel: 3}),
+imagemin.jpegtran({progressive: true}),
+imagemin.svgo()
+]))
+.pipe(gulp.dest("build/img"));
+});
+
+gulp.task("webp", function () {
+ return gulp.src("source/img/**/*.{png,jpg}")
+ .pipe(webp({quality: 90}))
+ .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("copy", function () {
+ return gulp.src([
+ "source/fonts/**/*.{woff,woff2}",
+ "source/img/**",
+ "source/js/**"
+ ], {
+ base: "source"
+ })
+ .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+ return del("build");
+});
+
+gulp.task("build", gulp.series(
+"clean",
+"copy",
+"css"
+));
+gulp.task("start", gulp.series(
+ "build",
+ "server"
+));
